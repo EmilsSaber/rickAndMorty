db908168b97688018ec3151a0e713b15393ce009
Emil <emil.mametov.01@gmail.com>
2023-02-14 14:38:08 +0600

    add view pager

--- app/src/main/java/com/example/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.rickandmorty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rickandmorty.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupViewPager()
+    }
+    private fun setupViewPager() {
+        val viewPager = binding.viewPager
+        viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
+
     }
 }
---
+++ app/src/main/java/com/example/rickandmorty/ViewPagerAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.rickandmorty
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+
+class ViewPagerAdapter (fm: FragmentManager): FragmentPagerAdapter(fm){
+    private val COUNT = 3
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> CharactersFragment()
+            1 -> EpisodeFragment()
+            2 -> LocationFragment()
+            else -> CharactersFragment()
+        }
+    }
+
+    override fun getCount(): Int {
+        return COUNT
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        when (position) {
+            0 -> return "Персонажи"
+            1 -> return "Эпизод"
+            2 -> return "Локации"
+        }
+        return ""
+    }
+
+}
